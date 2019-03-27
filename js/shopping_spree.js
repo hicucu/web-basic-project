ee66571e497aaf3a729877da18fea748f922cdda
hicucu <hicucu@gmail.com>
2019-03-27 22:26:51 +0900

    feat: hover, essential, card icon add
    footer sns icon hover add
    input modal essential validation
    card icon add
    card hover add

--- js/shopping_spree.js
+++ js/shopping_spree.js
@@ -6,10 +6,10 @@ var _listCount = 0;
 $(document).ready(function () {
     setMinHeight();
     loadShoppingList();
-    
+
     $(document).on('click', '#uk-button-save', function (e) {
-        
-        buttonClickSave();
+
+        buttonClickSave(e);
     });
 });
 
@@ -27,7 +27,7 @@ function loadShoppingList() {
     shoppingListStringify = decodeURIComponent(shoppingListStringify);
     var _shoppingList = JSON.parse(shoppingListStringify);
 
-    for (var index in _shoppingList) {        
+    for (var index in _shoppingList) {
         var shoppingItem = JSON.parse(_shoppingList[index]);
 
         addShoppingItme(shoppingItem);
@@ -50,90 +50,124 @@ function addShoppingItme(shoppingItem) {
         buttonHtml = '<h4 class="inline-block"><span class="uk-label-danger sort-red">시발비용</span></h4>';
     else if (priority == "꽁돈생기면")
         buttonHtml = '<h4 class="inline-block"><span class="uk-label-warning sort-yellow">꽁돈생기면</span></h4>';
-    else buttonHtml='<h4 class="inline-block"><span class="uk-label-success sort-green">고민 중</span></h4>';
+    else buttonHtml = '<h4 class="inline-block"><span class="uk-label-success sort-green">고민 중</span></h4>';
 
 
 
     var card =
-        '<div class="shopping-card uk-card uk-card-default uk-card-body">' +
-            '<h3 class="uk-card-title" id="card-title">'+title+'</h3>' +
-            '<span class="card-title-close" uk-icon="close">'+
-            '</span>'+
-            '<span id="card-main-date" class="uk-badge">'+date+'</span>' +
-            '<div class="uk-grid-small" uk-grid>' +
-                '<div class="uk-width-expand" uk-leader="fill: -">' +
-                    buttonHtml+
-                '</div>' +
-                '<div id="card-main-price">'+price+'</div>' +
-            '</div>' +
-            '<div id="card-hide">' +path+'</div>' +
-            '<div id="card-hide">' +reason+'</div>' +
+        '<div class="shopping-card uk-card uk-card-default uk-card-body ">' +
+        '<h3 class="uk-card-title" id="card-title">' + title + '</h3>' +
+        '<div id="card-icon-group">' +
+        '<span class="card-title-comment card-icon btns" uk-icon="comment">' +
+        '</span>' +
+        '<span class="card-title-edit card-icon btns" uk-icon="file-edit">' +
+        '</span>' +
+        '<span class="card-title-close card-icon btns" uk-icon="trash">' +
+        '</span>' +
+        '</div>' +
+        '<span id="card-main-date" class="uk-badge">' + date + '</span>' +
+        '<div id="card-mid" class="uk-grid-small" uk-grid>' +
+        '<div id="card-fill" class="uk-width-expand" uk-leader="fill: -">' +
+        buttonHtml +
+        '</div>' +
+        '<div id="card-main-price">' + price + '</div>' +
+        '</div>' +
+        '<div id="card-path" class="card-hide">' + path + '</div>' +
+        '<div id="card-reason" class="card-hide">' + reason + '</div>' +
         '</div>';
 
     $('#card-list').append(card);
 
     $(".uk-card-body").off();
-    $(".uk-card-body").on("click", function(e){        
-        var path = $(this)[0].children[4].innerText;        
+    $(".uk-card-body").on("click", function (e) {
+        var path = $(this)[0].children[4].innerText;
         window.open(path);
     });
     $('.card-title-close').off();
-    $('.card-title-close').on("click", function(e){
-        $(this).parent().off();        
-        $(this).parent().remove();
+    $('.card-title-close').on("click", function (e) {
+        $(this).parent().parent().off();
+        $(this).parent().parent().remove();
         saveShoppingList();
     });
 
-    
+
 }
 
-function saveShoppingList(){
+function saveShoppingList() {
     var count;
     var _shoppingList = {};
     var cards = $('.shopping-card');
     var shoppingItem = {};
     _listCount = cards.length;
-    for(var i=0; i<cards.length; i++){
+    for (var i = 0; i < cards.length; i++) {
         var shoppingItem = {};
-        shoppingItem["title"]=$('.shopping-card')[i].children[0].innerText;
-        shoppingItem["path"]=$('.shopping-card')[i].children[4].innerText;
-        shoppingItem["price"]=$('.shopping-card')[i].children[3].children[1].innerText;
-        shoppingItem["date"]=$('.shopping-card')[i].children[2].innerText;
-        shoppingItem["priority"]=$('.shopping-card')[i].children[3].children[0].innerText;
-        shoppingItem["reason"]=$('.shopping-card')[i].children[5].innerText
-        
-        var stringify = JSON.stringify(shoppingItem);
-        _shoppingList[i]=stringify;
+        shoppingItem["title"] = $('.shopping-card')[i].children[id = "card-title"].innerText;
+        shoppingItem["path"] = $('.shopping-card')[i].children[id = "card-path"].innerText;
+        shoppingItem["price"] = $('.shopping-card')[i].children[id = "card-mid"].children[id = "card-main-price"].innerText;
+        shoppingItem["date"] = $('.shopping-card')[i].children[id = "card-main-date"].innerText;
+        shoppingItem["priority"] = $('.shopping-card')[i].children[id = "card-mid"].children[id = "card-fill"].innerText;
+        shoppingItem["reason"] = $('.shopping-card')[i].children[id = "card-reason"].innerText;
 
-    }    
+        var stringify = JSON.stringify(shoppingItem);
+        _shoppingList[i] = stringify;
+
+    }
 
     var listStringify = JSON.stringify(_shoppingList);
     localStorage.setItem(_shoppingListKey, listStringify);
-    
+
 
 }
-function buttonClickSave(){
-    var shoppingItem = {};
-    shoppingItem["title"]=$('#title').val();
-    shoppingItem["path"]=$('#path').val();
-    shoppingItem["price"]=$('#price').val();
-    shoppingItem["date"]=$('#date').val();
-    shoppingItem["priority"]=$('#priority').val();
-    shoppingItem["reason"]=$('#reason').val();
-
+function resetModal() {
     $('#title').val("");
     $('#path').val("");
     $('#price').val("");
     $('#date').val("");
     $('#priority').val("우선순위를 선택하세요");
     $('#reason').val("");
-    
+}
+function buttonClickSave(e) {
+    var shoppingItem = {};
+
+    if ($('#title').val() == "" || $('#path').val() == "") {
+        $('#modal-container').addClass("uk-open");
+        resetModal();
+        return;
+    }
+    var today=$('#date').val();
+    if ( today == "") {
+        var date = new Date();
+        today =
+            leadingZeros(date.getFullYear(), 4) + '-' +
+            leadingZeros(date.getMonth() + 1, 2) + '-' +
+            leadingZeros(date.getDate(), 2);
+    }
+
+    shoppingItem["title"] = $('#title').val();
+    shoppingItem["path"] = $('#path').val();
+    shoppingItem["price"] = $('#price').val();
+    shoppingItem["date"] = today;
+    shoppingItem["priority"] = $('#priority').val();
+    shoppingItem["reason"] = $('#reason').val();
+
+    resetModal();
 
     addShoppingItme(shoppingItem);
     saveShoppingList();
 }
 
-$('#filter-button-blue').click(function () { 
+function leadingZeros(n, digits) {
+    var zero = '';
+    n = n.toString();
+
+    if (n.length < digits) {
+        for (i = 0; i < digits - n.length; i++)
+            zero += '0';
+    }
+    return zero + n;
+}
+
+$('#filter-button-blue').click(function () {
 
     $('.sort-blue').parent().parent().parent().parent().parent().show();
     $('.sort-red').parent().parent().parent().parent().parent().hide();
